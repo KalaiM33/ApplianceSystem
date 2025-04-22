8390073700a0ba350beb759e9687a88cd2377f61
kalaivani <88323462+KalaiM33@users.noreply.github.com>
2025-04-22 19:20:24 -0300

    Update Light.java

--- src/main/java/com/example/demo/model/Light.java
+++ src/main/java/com/example/demo/model/Light.java
@@ -2,8 +2,7 @@ package com.example.demo.model;
 
 public class Light {
     private boolean on = false;
-    public void toggle() { on = !on;
-    System.out.println("Light is " + isOn());}
+    public void toggle() { on = !on;}
     public void turnOff() { on = false; }
     public boolean isOn() { return on; }
     public String message ="Light is OFF";
